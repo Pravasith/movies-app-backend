24b719cbbaf1aa03d89c7afe34d75db6efeb17ce
pravasith <pravasith@gmail.com>
2022-07-19 12:25:43 -0400

    Spring starter

---
+++ src/main/java/com/moviestore/cjv/CjvApplication.java
@@ -0,0 +1,13 @@
+package com.moviestore.cjv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CjvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CjvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moviestore/cjv/CjvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviestore.cjv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CjvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
