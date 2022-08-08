4b3bf6d493d0551af5c731ae4e52bc8785bb2ac0
pravasith <pravasith@gmail.com>
2022-08-07 21:34:00 -0400

    added cors

--- src/main/java/com/moviestore/cjv/CjvApplication.java
+++ src/main/java/com/moviestore/cjv/CjvApplication.java
@@ -2,6 +2,9 @@ package com.moviestore.cjv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CjvApplication {
@@ -10,4 +13,13 @@ public class CjvApplication {
 		SpringApplication.run(CjvApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
 }
